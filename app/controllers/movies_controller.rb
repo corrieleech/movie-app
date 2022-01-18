978de469d99d8c65d3f2ba1f38fab113763ddfe5
Corrie <corrieleech@gmail.com>
2022-01-17 20:22:02 -0600

    adds restful routes for movies and actors

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,24 +3,54 @@ class MoviesController < ApplicationController
     movies = Movie.all
     render json: movies.to_json
   end
+  
+  def create
+    movie = Movie.new(
+      title: params[:title],
+      year: params[:year],
+      plot: params[:plot])
+    movie.save
+    render json: movie.to_json
+  end
+
   def show
     movie = Movie.find(params[:id])
     render json: movie.to_json
   end
+  
+  def update
+    movie = Movie.find(params[:id])
+    movie.title = params[:title] || movie.title
+    movie.year = params[:year] || movie.year
+    movie.plot = params[:plot] || movie.plot
+    movie.save
+    render json: movie.to_json
+  end
+
+  def destroy
+    movie = Movie.find(params[:id])
+    movie.destroy
+    render json: {message: "The movie has been removed from the database."}
+  end
+
   def alphabetical
     movies = Movie.order("title")
     render json: movies.to_json
   end
+  
   def five_recent
     movies = Movie.order(year: :desc).limit(5)
     render json: movies.to_json
   end
+  
   def next_five_recent
     movies = Movie.order(year: :desc).offset(5)
     render json: movies.to_json
   end
+  
   def eighties
     movies = Movie.all.select {|movie| movie.year >= 1980 && movie.year < 1990 }
     render json: movies
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  get "/actors" => "actors#index"
+  post "/actors" => "actors#create"
+  get "/actors/:id" => "actors#show"
+  patch "/actors/:id" => "actors#update"
+  delete "/actors/:id" => "actors#destroy"
+  
   get "/movies" => "movies#index"
+  post "/movies" => "movies#create"
   get "/movies/:id" => "movies#show"
+  patch "/movies/:id" => "movies#update"
+  delete "/movies/:id" => "movies#destroy"
+  
   get "/movies_a_to_z" => "movies#alphabetical"
   get "/newest_5_movies" => "movies#five_recent"
   get "/newest_10_movies" => "movies#next_five_recent"
   get "/eighties_movies" => "movies#eighties"
-  # get "/actors/" => "actors#show" #Commented out so that the url paramater path works.
-  get "/actors/:id" => "actors#show"
-  post "actors/" => "actors#show"
 end
