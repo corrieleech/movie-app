764a97b68504b0d5f8bdd634ae805f606971a081
Corrie <corrieleech@gmail.com>
2022-02-09 10:02:47 -0600

    edits cors file to allow localhost8080 connection to front end and temp removes authorization from movies controller for testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
 class MoviesController < ApplicationController
-  before_action :authenticate_admin, only: [:create, :update, :destroy]
+  # before_action :authenticate_admin, only: [:create, :update, :destroy]
   
   def index
     movies = Movie.where("english = true")
